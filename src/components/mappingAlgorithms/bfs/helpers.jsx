eab384983fb37a671e614e6081d47da11f0d67b8
Ofek Peres <operes@princeton.edu>
2021-12-17 11:31:22 -0500

    added bfs visualization! woohoo

---
+++ src/components/mappingAlgorithms/bfs/helpers.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import Slider from '../../ui/slider';
+function StartNodeSlider({ value, setValue, min, max, step, color }) {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <p>The Start Node Radius is: {value}</p>
+      <Slider
+        value={value}
+        setValue={setValue}
+        min={min}
+        max={max}
+        step={step}
+        color={color}
+      ></Slider>
+    </div>
+  );
+}
+
+function GoalNodeSlider({ value, setValue, min, max, step, color }) {
+  return (
+    <div>
+      <p>The Goal Area Radius is: {value}</p>
+      <Slider
+        value={value}
+        setValue={setValue}
+        min={min}
+        max={max}
+        step={step}
+        color={color}
+      ></Slider>
+    </div>
+  );
+}
+
+function CircleRadiusSlider({ value, setValue, min, max, step, color }) {
+  return (
+    <div>
+      <p>Set the radius of the circle obstacles: {value}</p>
+      <Slider
+        value={value}
+        setValue={setValue}
+        min={min}
+        max={max}
+        step={step}
+        color={color}
+      ></Slider>
+    </div>
+  );
+}
+
+function StepSizeRadiusSlider({ value, setValue, min, max, step, color }) {
+  return (
+    <div>
+      <p>Set the max step size for the RRT algorithm: {value}</p>
+      <Slider
+        value={value}
+        setValue={setValue}
+        min={min}
+        max={max}
+        step={step}
+        color={color}
+      ></Slider>
+    </div>
+  );
+}
+
+function RectangleSizeSlider({
+  width,
+  height,
+  setWidth,
+  setHeight,
+  min,
+  max,
+  step,
+  color,
+}) {
+  return (
+    <div>
+      <h3 className="text-lg font-bold">
+        Set the Dimensions of the Rectangular Obstacles
+      </h3>
+      <p>Set the Width: {width}</p>
+      <Slider
+        value={width}
+        setValue={setWidth}
+        min={min}
+        max={max}
+        step={step}
+        color={color}
+      ></Slider>
+      <p>Set the Height: {height}</p>
+      <Slider
+        value={height}
+        setValue={setHeight}
+        min={min}
+        max={max}
+        step={step}
+        color={color}
+      ></Slider>
+    </div>
+  );
+}
+
+export {
+  StartNodeSlider,
+  GoalNodeSlider,
+  RectangleSizeSlider,
+  CircleRadiusSlider,
+  StepSizeRadiusSlider
+};
--- src/components/mappingAlgorithms/index.jsx
+++ src/components/mappingAlgorithms/index.jsx
@@ -1,17 +1,20 @@
 import React, { useState, useEffect, useRef } from 'react';
 import MyListbox from '../ui/listbox';
+import BFSVisualization from './bfs';
 import RRTVisualization from './rrt';
 const MAPPING_ALGORITHM_OPTIONS = [
   { id: 1, value: 'RRT', unavailable: false },
-  { id: 2, value: 'RRT *', unavailable: false },
   { id: 3, value: 'BFS', unavailable: false },
+  { id: 2, value: 'RRT *', unavailable: false },
 ];
 export default function MappingAlgorithms() {
   const [selectedMappingAlgorithm, setSelectedMappingAlgorithm] =
     useState('RRT');
   return (
     <div className="flex flex-col items-center text-gray-800">
-      <p className="text-xs lg:text-xl text-gray-800 font-bold">Select a Mapping Algorithm!</p>
+      <p className="text-xs lg:text-xl text-gray-800 font-bold">
+        Select a Mapping Algorithm!
+      </p>
       <MyListbox
         options={MAPPING_ALGORITHM_OPTIONS}
         updateParentSelection={setSelectedMappingAlgorithm}
@@ -19,8 +22,8 @@ export default function MappingAlgorithms() {
       {
         {
           RRT: <RRTVisualization />,
+          BFS: <BFSVisualization />,
           'RRT *': <h1>RRT *</h1>,
-          BFS: <h1>BFS</h1>,
         }[selectedMappingAlgorithm.value]
       }
     </div>
